1f0593f0f5104b667fd25cb77aee42a1014abc22
Nguyễn Thành Thái <nguyentthai96@hotmail.com>
2016-05-26 19:21:56 +0700

    Main form sửa sơ

--- WFAStudentPlanManagement/FMain.Designer.cs
+++ WFAStudentPlanManagement/FMain.Designer.cs
@@ -31,10 +31,10 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FMain));
             this.pnlTool = new DevExpress.XtraEditors.PanelControl();
-            this.btnAccount = new DevExpress.XtraEditors.SimpleButton();
-            this.xtraTabbedMdiManager = new DevExpress.XtraTabbedMdi.XtraTabbedMdiManager(this.components);
+            this.btnAddPlan = new DevExpress.XtraEditors.SimpleButton();
             this.lblStudentName = new DevExpress.XtraEditors.LabelControl();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnHome = new DevExpress.XtraEditors.SimpleButton();
+            this.xtraTabbedMdiManager = new DevExpress.XtraTabbedMdi.XtraTabbedMdiManager(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pnlTool)).BeginInit();
             this.pnlTool.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabbedMdiManager)).BeginInit();
@@ -45,9 +45,9 @@
             this.pnlTool.Appearance.BackColor = System.Drawing.SystemColors.Control;
             this.pnlTool.Appearance.Options.UseBackColor = true;
             this.pnlTool.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.pnlTool.Controls.Add(this.simpleButton1);
+            this.pnlTool.Controls.Add(this.btnAddPlan);
             this.pnlTool.Controls.Add(this.lblStudentName);
-            this.pnlTool.Controls.Add(this.btnAccount);
+            this.pnlTool.Controls.Add(this.btnHome);
             this.pnlTool.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.pnlTool.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTool.Location = new System.Drawing.Point(0, 0);
@@ -55,26 +55,20 @@
             this.pnlTool.Size = new System.Drawing.Size(835, 85);
             this.pnlTool.TabIndex = 0;
             // 
-            // btnAccount
+            // btnAddPlan
             // 
-            this.btnAccount.AppearancePressed.BackColor = System.Drawing.Color.Blue;
-            this.btnAccount.AppearancePressed.Options.UseBackColor = true;
-            this.btnAccount.AutoWidthInLayoutControl = true;
-            this.btnAccount.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAccount.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnAccount.Image = ((System.Drawing.Image)(resources.GetObject("btnAccount.Image")));
-            this.btnAccount.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
-            this.btnAccount.Location = new System.Drawing.Point(0, 0);
-            this.btnAccount.Name = "btnAccount";
-            this.btnAccount.Size = new System.Drawing.Size(85, 85);
-            this.btnAccount.TabIndex = 0;
-            this.btnAccount.Tag = "Sign In";
-            this.btnAccount.ToolTip = "Account Sign In";
-            this.btnAccount.Click += new System.EventHandler(this.btnAccount_Click);
-            // 
-            // xtraTabbedMdiManager
-            // 
-            this.xtraTabbedMdiManager.MdiParent = this;
+            this.btnAddPlan.AppearancePressed.BackColor = System.Drawing.Color.Blue;
+            this.btnAddPlan.AppearancePressed.Options.UseBackColor = true;
+            this.btnAddPlan.AutoWidthInLayoutControl = true;
+            this.btnAddPlan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddPlan.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnAddPlan.Image = ((System.Drawing.Image)(resources.GetObject("btnAddPlan.Image")));
+            this.btnAddPlan.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnAddPlan.Location = new System.Drawing.Point(85, 0);
+            this.btnAddPlan.Name = "btnAddPlan";
+            this.btnAddPlan.Size = new System.Drawing.Size(85, 85);
+            this.btnAddPlan.TabIndex = 2;
+            this.btnAddPlan.Tag = "Add Plan Event";
             // 
             // lblStudentName
             // 
@@ -87,19 +81,26 @@
             this.lblStudentName.TabIndex = 1;
             this.lblStudentName.Text = "Hello Nguyễn Thành Thái";
             // 
-            // simpleButton1
+            // btnHome
             // 
-            this.simpleButton1.AppearancePressed.BackColor = System.Drawing.Color.Blue;
-            this.simpleButton1.AppearancePressed.Options.UseBackColor = true;
-            this.simpleButton1.AutoWidthInLayoutControl = true;
-            this.simpleButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.simpleButton1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.simpleButton1.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.Image")));
-            this.simpleButton1.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
-            this.simpleButton1.Location = new System.Drawing.Point(85, 0);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(85, 85);
-            this.simpleButton1.TabIndex = 2;
+            this.btnHome.AppearancePressed.BackColor = System.Drawing.Color.Blue;
+            this.btnHome.AppearancePressed.Options.UseBackColor = true;
+            this.btnHome.AutoWidthInLayoutControl = true;
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnHome.Location = new System.Drawing.Point(0, 0);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(85, 85);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Tag = "Page Home";
+            this.btnHome.ToolTip = "Account Sign In";
+            this.btnHome.Click += new System.EventHandler(this.btnAccount_Click);
+            // 
+            // xtraTabbedMdiManager
+            // 
+            this.xtraTabbedMdiManager.MdiParent = this;
             // 
             // FMain
             // 
@@ -109,7 +110,7 @@
             this.Controls.Add(this.pnlTool);
             this.IsMdiContainer = true;
             this.Name = "FMain";
-            this.Text = "FMain";
+            this.Text = "Student Managerment Study";
             ((System.ComponentModel.ISupportInitialize)(this.pnlTool)).EndInit();
             this.pnlTool.ResumeLayout(false);
             this.pnlTool.PerformLayout();
@@ -121,9 +122,9 @@
         #endregion
 
         private DevExpress.XtraEditors.PanelControl pnlTool;
-        private DevExpress.XtraEditors.SimpleButton btnAccount;
+        private DevExpress.XtraEditors.SimpleButton btnHome;
         private DevExpress.XtraEditors.LabelControl lblStudentName;
         private DevExpress.XtraTabbedMdi.XtraTabbedMdiManager xtraTabbedMdiManager;
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton btnAddPlan;
     }
 }
